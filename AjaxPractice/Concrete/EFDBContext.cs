6355d825922ffc07b7ba85b4a3b82296521323fa
Muroxxas <lavarnwayj@thomas.edu>
2020-03-03 12:28:10 -0500

    Base project set up

---
+++ AjaxPractice/Concrete/EFDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using AjaxPractice.Models;
+
+namespace AjaxPractice.Concrete
+{
+    public class EFDBContext
+    {
+        public DbSet<EmpModel> Emps { get; set; }
+    }
+}
--- AjaxPractice/Controllers/HomeController.cs
+++ AjaxPractice/Controllers/HomeController.cs
@@ -3,28 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AjaxPractice.Models;
+using AjaxPractice.Concrete;
+using System.Data.Entity;
+using System.Net;
 
 namespace AjaxPractice.Controllers
 {
     public class HomeController : Controller
     {
+
+        private EFDBContext db;
+
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Employee()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
     }
 }
