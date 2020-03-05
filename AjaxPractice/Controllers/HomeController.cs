f8593d81458a3ec22e7f3028de99ea6b4b15126c
Muroxxas <lavarnwayj@thomas.edu>
2020-03-05 10:56:18 -0500

    hmmm

--- AjaxPractice/Controllers/HomeController.cs
+++ AjaxPractice/Controllers/HomeController.cs
@@ -17,14 +17,44 @@ namespace AjaxPractice.Controllers
 
         public ActionResult Index()
         {
+            ViewBag.Employees = GetEmpModels();
             return View();
         }
 
-        public ActionResult Employee()
+
+
+        public ActionResult Employee(int? id)
         {
 
             return View();
         }
-        
+        public PartialViewResult Alphabetical()
+        {
+            //Returns alphabetically.
+            List<EmpModel> model = db.Emps.OrderBy(x => x.Name).ToList();
+            return PartialView("_BLAHBLAHBLAH", model);
+        }
+
+
+        public PartialViewResult All()
+        {
+            //Get by key order.
+            List<EmpModel> model = db.Emps.ToList();
+            return PartialView("_BLAHBLAHBLAH", model);
+        }
+
+        private IQueryable<EmpModel> GetEmpModels ()
+        {
+            var Employees = from Employee in db.Emps
+                             select new EmpModel
+                             {
+                                 Name = Employee.Name,
+                                 City = Employee.City,
+                                 Address = Employee.Address
+                            };
+            return Employees;
+}
+
     }
 }
+
